ee5344ff852146cc0d7cfcd313eff16f81755b96
Don_israel <israelojakorotu@gmail.com>
2024-11-13 21:02:57 +0100

    due amount

---
+++ main.py
@@ -0,0 +1,9 @@
+# Define the amount paid by Vishal and the total bill amount
+amount_paid = 4.00
+bill_amount = 2.50
+
+# Calculate the amount to return
+amount_to_return = amount_paid - bill_amount
+
+# Display the result
+print("The shopkeeper should return $", amount_to_return)
